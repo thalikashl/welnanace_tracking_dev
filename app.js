5d2415b0f064b489f1f51fa41a452f3b23f32530
thalikashl <thalika-shl@welnance.com>
2021-12-09 16:48:08 +0700

    ko

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 
 const express = require ('express')
 const app = express()
-const port = 3000
+const port = 8080
 
 
 // listen on port 3000
 
 app.listen(port, () => console.info('listening on port  ${port}'))
+app.listen(3000, function () { console.log('Listening on port 3000!') })
 
 //static flie
 
@@ -15,6 +16,8 @@ app.use(express.static('pubilc'))
 app.use('style', express.static(__dirname + 'pubilc/style'))
 app.use('img', express.static(__dirname + 'pubilc/img'))
 
+
+
 app.get('', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.render('index')
 })
